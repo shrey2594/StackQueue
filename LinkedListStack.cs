6b03a65c0aa7acb479357c074de735431de196f8
shrey2594 <shrey2510@gmail.com>
2022-04-10 11:01:57 +0530

    Data Structure stack implementation

---
+++ LinkedListStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public class LinkedListStack
+    {
+        //private Node first;
+        private Node last;
+        public LinkedListStack()
+        {
+            last = null;
+        }
+        public void push(int data)
+        {
+            Node newNode = new Node(data);
+            if(last == null)
+            {
+                last = newNode;
+            }
+            else
+            {
+                newNode.next = last;
+                last=newNode;
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("\nDisplaying added nodes");
+            Node temp = this.last;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write("" + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            LinkedListStack stack = new LinkedListStack();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.display();    
+        }
+    }
+
+}
