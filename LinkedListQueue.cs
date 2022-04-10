39eab755a1d356bf05e55387d937cde88513279d
shrey2594 <shrey2510@gmail.com>
2022-04-10 18:47:00 +0530

    Queue UC4 Assignment

---
+++ LinkedListQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    public class LinkedListQueue
+    {
+        private Node last;
+        private Node first;
+        public LinkedListQueue()
+        {
+            last = null;
+            first = null;
+        }
+        public void enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (last == null && first == null)
+            {
+                last = newNode;
+                first = newNode;
+            }
+            else
+            {
+                last.next = newNode;
+                last = newNode;
+            }
+        }
+        public void dequeue()
+        {
+            while(first != null)
+            {
+                Console.WriteLine("Queue is not empty and the value to be dequeued is:" + this.first.data);
+                first = first.next;
+                display();
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("\nDisplaying nodes");
+            Node temp = this.first;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write("" + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace StackQueue
         {
             Console.WriteLine("Hello World");
             LinkedListStack stack = new LinkedListStack();
+            LinkedListQueue queue = new LinkedListQueue();
             stack.push(70);
             stack.push(30);
             stack.push(56);
@@ -15,6 +16,12 @@ namespace StackQueue
             stack.push(493);
             stack.display();
             stack.pop();
+            queue.enqueue(56);
+            queue.enqueue(30);
+            queue.enqueue(70);
+            queue.display();
+            queue.dequeue();
+
         }
     }
 
